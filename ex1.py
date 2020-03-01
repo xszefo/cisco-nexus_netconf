281ab77327194511e584b1cb9a88604e635a413c
root <root@ip-178-216-201-176.e24cloud.com>
2020-03-01 18:50:56 +0100

    Init with constants

---
+++ ex1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+
+
+def main():
+    # CONSTANTS
+    host = 'sbx-nxos-mgmt.cisco.com'
+    netconf_port = '8181'
+    username = 'admin'
+    password = 'Admin_1234!'
+
+
+if __name__ == '__main__':
+    main()
